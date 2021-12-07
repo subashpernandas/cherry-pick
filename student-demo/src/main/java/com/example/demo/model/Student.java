a7f23226a4f2d79d4f6a82187141c18cc9cde161
subashpernandas <84831317+subashpernandas@users.noreply.github.com>
2021-12-07 17:30:29 +0530

    address getter setter

--- student-demo/src/main/java/com/example/demo/model/Student.java
+++ student-demo/src/main/java/com/example/demo/model/Student.java
@@ -30,6 +30,13 @@ public class Student {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String  address) {
+		this.address = address;
+	}
 
 	public Integer getAge() {
 		return age;
