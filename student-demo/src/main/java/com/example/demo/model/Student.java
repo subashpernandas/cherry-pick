0bb30b59d43bec681dce543658306abdf2b6b9d5
Subash BERDINANT <subhashb.in@mouritech.com>
2021-12-07 16:59:43 +0530

    age added

--- student-demo/src/main/java/com/example/demo/model/Student.java
+++ student-demo/src/main/java/com/example/demo/model/Student.java
@@ -6,6 +6,8 @@ public class Student {
 	
 	private String name;
 	
+	private Integer age;
+	
 	public Student(Integer id, String name) {
 		this.id = id;
 		this.name = name;
@@ -25,6 +27,14 @@ public class Student {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
 	}
 
 }
